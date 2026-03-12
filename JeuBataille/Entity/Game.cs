76943ea4760f44ece2b5c80177f8b8ea6c4cc4f7
De Nève Alexandre <alexdeneve@hotmail.be>
2026-03-12 16:42:08 +0100

    Ajout de l'entité Game, création du paquet avec les 52 cartes

---
+++ JeuBataille/Entity/Game.cs
@@ -0,0 +1,26 @@
+﻿using JeuBataille.Enum;
+
+namespace JeuBataille.Entity;
+
+public class Game
+{
+    private List<Carte> deck = new List<Carte>();
+
+    public Game()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        foreach (EnumCouleur couleur in EnumCouleur.GetValues<EnumCouleur>())
+        {
+            foreach (EnumCarteValeurs valeur in EnumCarteValeurs.GetValues<EnumCarteValeurs>())
+            {
+                deck.Add(new Carte(couleur, valeur));
+            }
+        }
+
+        deck = deck.OrderBy(x => Guid.NewGuid()).ToList();
+    }
+}
--- JeuBataille/Program.cs
+++ JeuBataille/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using JeuBataille.Entity;
 
-Console.WriteLine("Hello, World!");
+Game game = new Game();
