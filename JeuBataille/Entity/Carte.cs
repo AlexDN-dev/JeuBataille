33890da26df0c769474d438a4244d3b74d9db9c7
De Nève Alexandre <alexdeneve@hotmail.be>
2026-03-12 16:18:48 +0100

    Ajout de l'entité carte

---
+++ JeuBataille/Entity/Carte.cs
@@ -0,0 +1,20 @@
+﻿using JeuBataille.Enum;
+
+namespace JeuBataille.Entity;
+
+public class Carte
+{
+    private EnumCouleur Couleur { get; set; }
+    private EnumCarteValeurs Valeur { get; set; }
+
+    public Carte(EnumCouleur couleur, EnumCarteValeurs valeur)
+    {
+        Couleur = couleur;
+        Valeur = valeur;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.Write($"{Valeur} de {Couleur}");
+    }
+}
---
+++ JeuBataille/Enum/EnumCarte.cs
@@ -0,0 +1,26 @@
+﻿namespace JeuBataille.Enum;
+
+public enum EnumCouleur
+{
+    Coeur,
+    Trèfle,
+    Pique,
+    Carreau
+}
+
+public enum EnumCarteValeurs
+{
+    Deux = 2,
+    Trois,
+    Quatre,
+    Cinq,
+    Six,
+    Sept,
+    Huit,
+    Neuf,
+    Dix,
+    Valet,
+    Dame,
+    Roi,
+    As
+}
